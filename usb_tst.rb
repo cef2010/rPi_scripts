c6a74a8c33b071f99a34f71a3cb72cb1d963d56f
cef2010 <cef2010@gmail.com>
2016-03-17 09:53:31 -0600

    adds support for midifighter in usb_tst

--- usb_tst.rb
+++ usb_tst.rb
@@ -3,11 +3,16 @@ require "libusb"
 
 
 class Controller
-  def initialize
+  def initialize(idVendor, idProduct, endpoint, dataIn, timeout)
+    @idVendor = idVendor
+    @idProduct = idProduct
+    @endpoint = endpoint
+    @dataIn = dataIn
+    @timeout = timeout
     @usb_context = LIBUSB::Context.new
     @device = @usb_context.devices(
-      idVendor: 121,
-      idProduct: 6
+      idVendor: @idVendor,
+      idProduct: @idProduct
     ).first
     reset_device_access
     @handle = @device.open
@@ -23,9 +28,9 @@ class Controller
 
   def raw_data
     data = @handle.interrupt_transfer(
-      endpoint: 129,
-      dataIn: 7,
-      timeout: 10
+      endpoint: @endpoint,
+      dataIn: @dataIn,
+      timeout: @timeout
     )
     data.bytes
   end
@@ -35,7 +40,8 @@ end
 # out => address: 1, interval: 10, length: 7
 # in => address: 129, interval: 10, length: 7
 
-c = Controller.new
+# c = Controller.new(121, 6, 129, 7, 10) #n64
+c = Controller.new(0x2580, 0x0005, 130, 9, 0) #midifighter
 
 loop do
   begin
